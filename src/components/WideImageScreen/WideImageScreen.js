1c550e244df1e2d1cbd8b99126bd58c4296df7d6
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-10-13 23:38:55 +0530

    [home] added text transitions

--- src/components/WideImageScreen/WideImageScreen.js
+++ src/components/WideImageScreen/WideImageScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import { motion } from "framer-motion";
+import AnimatedText from "../../global/AnimatedText"
 
 import './WideImageScreen.css';
 
@@ -9,8 +10,22 @@ function WideImageScreen() {
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
   const [rotate, setRotate] = useState(0);
-  const [xTitle, setXTitle] = useState(0);
-  const [yTitle, setYTitle] = useState(0);
+  const [replay, setReplay] = useState(true);
+
+  // Placeholder text data, as if from API
+  const placeholderText = [
+    { type: "heading1", text: "Sarah" },
+    { type: "heading2", text: "And"},
+    { type: "heading1", text: "Justin" }
+  ];
+
+  const container = {
+    visible: {
+      transition: {
+        staggerChildren: 0.025
+      }
+    }
+  };
 
   const background_image={
       background: 'linear-gradient(to right, #320000, #f68989)'
@@ -20,30 +35,30 @@ function WideImageScreen() {
       <div>
           <div  className="background_image" style={background_image}>
             <motion.div animate={{ x, y, rotate }} onMouseOver={() => {
-              setX(90);
+              setX(60);
               setY(-20);
               setRotate(-2);
-              setXTitle(50);
-              setYTitle(-50);
               }} onMouseOut={() => {
                 setX(5);
                 setY(5);
                 setRotate(-0.5);
-                setXTitle(-10);
-                setYTitle(-5);
               }}>  
               <img className="image" src={require("../../assets/images/01.jpg")} />
             </motion.div>
           </div>
-          <motion.div animate={{ xTitle, yTitle }} onMouseOver={() => {
-            setXTitle(50);
-            setYTitle(-50);
-            }} onMouseOut={() => {
-              setXTitle(-10);
-              setYTitle(-5);
-            }}>
-            <h1 className="home_title">Sarah <br></br> And <br></br> Justin</h1>
-          </motion.div>
+            <motion.div
+              className="App"
+              initial="hidden"
+              // animate="visible"
+              animate={replay ? "visible" : "hidden"}
+              variants={container}
+            >
+              <div className="home_title">
+                {placeholderText.map((item, index) => {
+                  return <AnimatedText className="text" {...item} key={index} />;
+                })}
+              </div>
+            </motion.div>
       </div>
   );
 }
