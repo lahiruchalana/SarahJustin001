2c86ac8498211062935db1349b8494cb5d03f5ff
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-10-18 23:07:22 +0530

    fixed issue on initial loading of transitions

--- src/views/RSVP/RSVP.js
+++ src/views/RSVP/RSVP.js
@@ -1,10 +1,30 @@
 import React from "react";
 
+import { motion, AnimatePresence } from "framer-motion";
+
+import './RSVP.css'
+
 function RSVP() {
 
     return(
         <div>
-            <h1>RSVP</h1>
+            <div className="background">
+                <motion.div  
+                    transition={{ repeat:Infinity, duration: 1 }} 
+                    initial="hidden" 
+                    whileInView="visible" 
+                    viewport={{ once: true }} 
+                    animate={{ x: 0, y: 0, opacity: 1 }}
+                    variants={{
+                        visible: { opacity: 1, scale: 1 },
+                        hidden: { opacity: 0, scale: 0 }
+                    }}
+                    >
+                        <h1 id="text_black" className="date">Be Ready In May 24th, 2023</h1>
+                        <h6 id="text_black" >@New York Hilton Midtown</h6>
+                        {/* <h4 id="text" >1335 Avenue Of The Americas, New York, NY 10019</h4> */}
+                </motion.div>
+            </div>
         </div>
     )
 }
