496ef13a03af197cb02aa754fe213589be8d886f
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-10-28 02:01:40 +0530

    delete this prop code

--- src/views/Events/Events.js
+++ src/views/Events/Events.js
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useState }  from "react";
+
+import { motion, AnimatePresence } from "framer-motion";
+
+import TextArea from "./TextArea";
+
 
 function Events() {
 
+    const [data, setData] = useState(null);
+
+    let num = 1;
+
+    const background={
+        background: 'linear-gradient(to right, #0e001a, #b364fd)'
+    }
+
+    function chnageValue() {
+        num = num + 1;
+        setData(`I need to pass this message to Accomodation page ${num*2}`)
+    }
+
+    console.log(data);
+
     return(
         <div>
-            <h1>Events</h1>
+            <div className="background" style={background}>
+                <br></br>
+                <br></br>
+                <br></br>
+                <AnimatePresence>
+                    <h1>Short and Sweet Story of Us</h1>
+                </AnimatePresence>
+
+
+
+
+
+                {/* some test code here */}
+                
+                <button onClick={() => chnageValue()}>Click to pass the value as Prop</button>
+
+                <br></br>
+
+                <TextArea propFromEvent={data}></TextArea>
+
+                {/* some test code here */}
+                
+            </div>
         </div>
     )
 }
--- src/views/OurStory/OurStory.js
+++ src/views/OurStory/OurStory.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 import YouTube from 'react-youtube';
+import { motion, AnimatePresence } from "framer-motion";
 
 import './OurStory.css'
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function OurStory() {
 
@@ -15,12 +19,84 @@ function OurStory() {
         },
     };
 
+    const background={
+        background: 'linear-gradient(to right, #0e001a, #b364fd)'
+    }
+
     return(
         <div>
+            <div className="background" style={background}>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <AnimatePresence>
+                    <Row>
+                        <Col>
+                            <motion.div  
+                                transition={{ duration: 3 }} 
+                                initial="hidden" 
+                                whileInView="visible" 
+                                viewport={{ once: true }} 
+                                animate={{ x: 0, y: 0, opacity: 1 }}
+                                variants={{
+                                    visible: { opacity: 1 },
+                                    hidden: { y: 100, opacity: 0 }
+                                }}
+                                >
+                                    <h2 id="text_white">Her Story</h2>
+                            </motion.div>
+                            <br></br>
+                            <motion.div  
+                                transition={{ duration: 3 }} 
+                                initial="hidden" 
+                                whileInView="visible" 
+                                viewport={{ once: true }} 
+                                animate={{ x: 0, y: 0, opacity: 1 }}
+                                variants={{
+                                    visible: { opacity: 1 },
+                                    hidden: { y: 100, opacity: 0 }
+                                }}
+                                >
+                                    <h5 id="text_white">His Story</h5>
+                            </motion.div>
+                        </Col>
+                        <Col>
+                            <motion.div  
+                                transition={{ duration: 3 }} 
+                                initial="hidden" 
+                                whileInView="visible" 
+                                viewport={{ once: true }} 
+                                animate={{ x: 0, y: 0, opacity: 1 }}
+                                variants={{
+                                    visible: { opacity: 1 },
+                                    hidden: { y: 100, opacity: 0 }
+                                }}
+                                >
+                                    <h2 id="text_white">His Story</h2>
+                            </motion.div>
+                            <br></br>
+                            <motion.div  
+                                transition={{ duration: 3 }} 
+                                initial="hidden" 
+                                whileInView="visible" 
+                                viewport={{ once: true }} 
+                                animate={{ x: 0, y: 0, opacity: 1 }}
+                                variants={{
+                                    visible: { opacity: 1 },
+                                    hidden: { y: 100, opacity: 0 }
+                                }}
+                                >
+                                    <h5 id="text_white">His Story</h5>
+                            </motion.div>
+                        </Col>
+                    </Row>
 
-            <h1>Dive in Our Story</h1>
-            {/* create a simple video for "our story" of them */}
-            <YouTube videoId="uQVqVpdhFhA" opts={opts} /> 
+                    <h1>Short and Sweet Story of Us</h1>
+                    {/* create a simple video for "our story" of them */}
+                    <YouTube videoId="uQVqVpdhFhA" opts={opts} /> 
+                </AnimatePresence>
+            </div>
         </div>
     )
 }
