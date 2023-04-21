169ad268bedfa5dc6ad5aac1b9d870d7af707ae0
Lahiru Chalana <lahiruchalanairesh@gmail.com>
2023-04-21 12:34:49 +0530

    home page changes

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -8,37 +8,54 @@ import WideImageScreen from "../../components/WideImageScreen/WideImageScreen.js
 
 function Home() {
 
+    const background_image={
+        background: 'linear-gradient(to right, #320000, #f68989)'
+    }
+
     return(
-        <div>
+        <div style={background_image}>
             <WideImageScreen/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
             <AnimatePresence>
                 <motion.div  
-                    transition={{ duration: 2.5 }} 
+                    transition={{ duration: 2 }} 
                     initial="hidden" 
                     whileInView="visible" 
                     viewport={{ once: true }} 
                     animate={{ x: 0, y: 0, opacity: 1 }}
                     variants={{
-                        visible: { opacity: 1, scale: 1 },
-                        hidden: { opacity: 0, scale: 0.5 }
+                        visible: { opacity: 1 },
+                        hidden: { y: 100, opacity: 0 }
                     }}
                     >
                     <h3 id="text_white" className="date">Be Ready In May 24th, 2023</h3>
                 </motion.div>
                 <motion.div  
-                    transition={{ duration: 4 }} 
+                    transition={{ duration: 3 }} 
                     initial="hidden" 
                     whileInView="visible" 
                     viewport={{ once: true }} 
                     animate={{ x: 0, y: 0, opacity: 1 }}
                     variants={{
-                        visible: { opacity: 1, scale: 1 },
-                        hidden: { opacity: 0, scale: 0.6 }
+                        visible: { opacity: 1 },
+                        hidden: { y: 100, opacity: 0 }
                     }}
                     >
                     <h6 id="text_white" >@New York Hilton Midtown</h6>
                     {/* <h4 id="text" >1335 Avenue Of The Americas, New York, NY 10019</h4> */}
                 </motion.div>
+                <br/>
+                <br/>
+                <br/>            
+                <br/>
+                <br/>
+                <br/>
+                
             </AnimatePresence>
         </div>
     );
